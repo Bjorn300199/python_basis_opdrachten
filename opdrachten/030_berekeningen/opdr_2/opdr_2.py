a795ecd1103c26fd1b2b030ce4378806dedd95d3
Bjorn300199 <97042566@st.deltion.nl>
2025-03-08 11:42:56 +0100

    Versie 1

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,14 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+C = -12
+F = 102
 
-print()
+celcius = (F - 32) / 1.8
+fahrenheit = (C * 1.8) + 32
+
+print(f"{C} graden Celcius is gelijk aan {fahrenheit} graden Fahrenheit")
+print(f"{F} graden fahrenheit is gelijk aan {celcius} graden Celcius")
+
+#-12 graden Celsius is gelijk aan 10.4 graden Fahrenheit  
+#102 graden Fahrenheit is gelijk aan 38.9 graden Celcius
