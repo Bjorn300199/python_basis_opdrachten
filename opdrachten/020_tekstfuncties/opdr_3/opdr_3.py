fa7b938b383a209c96247ff83cef6158287546a3
Bjorn300199 <97042566@st.deltion.nl>
2025-03-08 09:41:41 +0100

    init

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,18 @@
 # Groep:
 
 # Hier komt je code...
+
+
+tree = [
+ print("    *"),
+ print("   ***"),
+ print("  ******"),
+ print(" ********"),
+ print("***********"),
+ print("    ***"),
+ print("    ***"),
+ print("    ***")
+]
+
+for line in tree:
+    print (line * 5)
