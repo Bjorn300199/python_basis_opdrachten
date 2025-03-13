482a32f0d9198e2ec66adfcf7abd2f43c907ba7b
Bjorn300199 <Bjorn300199>
2025-03-13 14:41:50 +0100

    Versie 1

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -14,7 +14,3 @@ print(y)
 x = 0
 y = 4*(x**3) - 2*(x**2) - 1
 print(y)
-
-
-
-# y = 4x<sup>3</sup> - 2x<sup>2</sup> - 1
--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,12 @@
 # Opdracht 1 lists
 # Naam student:
 # Groep:
+#  print(lijst[?][?], lijst[?][?])
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+
+dict_1 = {"Firstname": "Ben", "Achternaam": "Dalen"}
+dict_2 = {"Firstname": "Mike", "Achternaam": "Pot"}
+dict_3 = {"Firstname": "Bjorn", "Achternaam": "Koerhuis"}
+dict_4 = {"Firstname": "Tom", "Achternaam": "Groen"}
+mylist = [dict_1, dict_2, dict_3, dict_4]
+print(mylist[1])
