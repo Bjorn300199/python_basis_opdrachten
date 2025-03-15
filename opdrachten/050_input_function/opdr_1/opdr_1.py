34d75b203237d11e32c49af079d551387a5e4c18
Bjorn300199 <97042566@st.deltion.nl>
2025-03-15 11:12:19 +0100

    Versie 1

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,19 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+import math
 
+lengte1 = 4
+lengte2 = 3
+schuinelengte = math.sqrt(lengte1**2 + lengte2**2)
+print(f"De schuine lengte is {schuinelengte}")
+
+lengte3 = 12
+lengte4 = 9
+schuinelengte2 = math.sqrt(lengte3**2 + lengte4**2)
+print(f"De schuinelengte is {schuinelengte2}")
+
+lengte5 = 11
+lengte6 = 9
+schuinelengte3 = math.sqrt(lengte5**2 + lengte6**2)
+print(f"De schuinelengte is {schuinelengte3}")
