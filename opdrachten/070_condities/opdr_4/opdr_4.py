87a058c885a5819ad50f838bd6e5a58c05a10a1a
Bjorn300199 <97042566@st.deltion.nl>
2025-03-24 23:02:13 +0100

    Versie 1

--- opdrachten/070_condities/opdr_4/opdr_4.py
+++ opdrachten/070_condities/opdr_4/opdr_4.py
@@ -3,10 +3,23 @@
 # Groep:
 
 
-
 toppings = [("olijven", 4.50), ("kaas", 3.50), ("salami", 3.00), ("pepperoni", 2.00) , ("ansjovis", 2.50)]
-beschikbare_toppings = ...
+beschikbare_toppings = [toppingname[0] for toppingname in toppings]
 
-keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings} \n")
+while True:
+    keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings} \n")
 
-# Hier de rest van jouw code...
+    price = 0
+    found = False
+
+    for toppingname, toppingprice in toppings:
+        if keuze == toppingname:
+            price = toppingprice
+            found = True
+            break
+
+    if found == True:
+        print(f"The price of {keuze} is: €{price:.2f}")
+        break
+    else:
+        print("Give a valid topping")
