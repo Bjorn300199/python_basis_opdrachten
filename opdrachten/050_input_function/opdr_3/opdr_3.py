9e55d9eda50b4f3e8d73c3a5c190b1c557ddba09
Bjorn300199 <97042566@st.deltion.nl>
2025-03-24 18:07:11 +0100

    Versie 1

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,13 @@
 
 # Hier komt je code...
 
+landen = []
+
+for i in range(5):
+    land = input(f"Land {i+1}:")
+    landen.append(land)
+
+landen.sort(reverse=True)
+
+print("Lijst met Landen:")
+print(landen)
