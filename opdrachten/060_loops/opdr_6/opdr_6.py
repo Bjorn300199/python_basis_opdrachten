75b84a5acf0d7fb11550bb5a9b1a98b529663bc1
Bjorn300199 <97042566@st.deltion.nl>
2025-03-24 21:28:24 +0100

    Versie 1

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,16 @@
 
 # Hier start de for-loop
 
-my_list = []
+my_list = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+my_list.sort()
+my_list.append("Peperoni")
+my_list.remove("verdi")
+
+print(my_list[0:3])
+
+middle = len(my_list) // 2
+middlevalue = my_list[middle]
+print(f"Middle value is: {middlevalue}")
+
+print(my_list[2:5])
+print(my_list)
