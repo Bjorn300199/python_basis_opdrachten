146db3d90fb4b8245c4f0fca7354ef8d38a31351
Bjorn300199 <97042566@st.deltion.nl>
2025-03-24 21:37:20 +0100

    Versie 1

--- opdrachten/070_condities/opdr_1/opdr_1.py
+++ opdrachten/070_condities/opdr_1/opdr_1.py
@@ -6,7 +6,12 @@
 
 # Hier start de for-loop....
 
-my_list = []
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in my_list:
+    if number > 4:
+        print(number)
+
 
 #for loop
 #if statement
