0335844495c9882dd180907fa7e4fa370bc58406
Bjorn300199 <97042566@st.deltion.nl>
2025-03-31 20:32:11 +0200

    Versie 1

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -5,6 +5,21 @@
 import random
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
-
+print(geheim_getal)
 # De rest moet jij doen.....
 
+print("Raad een getal tussen 1 en 100")
+pogingen = 0
+
+while True:
+    gok = int(input("Jouw gok: "))
+    pogingen += 1
+
+    if gok < geheim_getal:
+        print("Te laag")
+    elif gok > geheim_getal:
+        print("Te hoog")
+    else:
+        print(f"Juist geraden, het getal was {geheim_getal}")
+        print(f"Je hebt {pogingen} gedaan om het juist antwoord te krijgen")
+        break
