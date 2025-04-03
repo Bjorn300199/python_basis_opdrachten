89d039bd1bcaa322ccc3f6437b3d9db1b2d284d0
Bjorn300199 <97042566@st.deltion.nl>
2025-04-03 18:33:19 +0200

    versie 1

--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -4,9 +4,22 @@
 
 
 def volledige_naam(lijst_met_namen):
-    # hier komt jouw code
-    # Het woordje pass mag je weghalen
-    return namen
+    resultaat = []
+    
+    for persoon in lijst_met_namen:
+        voornaam = persoon["voornaam"]
+        tussenvoegsel = persoon["tussenvoegsel"]
+        achternaam = persoon["achternaam"]
+        
+        if tussenvoegsel:
+            naam = f"{voornaam} {tussenvoegsel} {achternaam}"
+        else:
+            naam = f"{voornaam} {achternaam}"
+        
+        resultaat.append(naam)
+        print(naam)
+    
+    return resultaat
 
 namen = [
     {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
